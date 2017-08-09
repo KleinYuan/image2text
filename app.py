79b8208e7fc3152bd149f736ca5cb15d7c0d2825
KleinYuan <kaiwen.yuan1992@gmail.com>
2017-08-08 22:50:34 -0700

    :fire: Hyperparam tuning ready

--- app.py
+++ app.py
@@ -9,19 +9,21 @@ embeddings_fp = '%s/embeddings.pickle' % generated_data_fp
 dict_vocabulary_fp = '%s/dict_vocabulary.pickle' % generated_data_fp
 reversed_dict_vocabulary_fp = '%s/reversed_dict_vocabulary.pickle' % generated_data_fp
 
-frozen_graph_filename='./vcnn-models/storyNet.pb'
-test_image_fp = './test-images/test.jpg'
+net_name = config.net_name
+frozen_graph_filename='./vcnn-models/%s.pb' % net_name
+test_image_fp = './test-images/test1.jpg'
 
 embeddings = load_pickle(embeddings_fp)
 dict_vocabulary = load_pickle(dict_vocabulary_fp)
 reversed_dict_vocabulary = load_pickle(reversed_dict_vocabulary_fp)
 
-predictionApp = PredictionApp(net_name='storyNet',
+predictionApp = PredictionApp(net_name=net_name,
                               embeddings=embeddings,
                               reversed_dict_vocabulary=reversed_dict_vocabulary,
                               dropout=True,
                               image_size=config.image_size,
-                              frozen_graph_filename=frozen_graph_filename)
+                              frozen_graph_filename=frozen_graph_filename,
+                              num_word2describe=config.num_word2describe)
 
 input_image = cv2.imread(test_image_fp)
 prediction = predictionApp.predict(input_image)
--- config/config.py
+++ config/config.py
@@ -1,4 +1,5 @@
 embedding_size = 128
-num_word2describe = 80
+num_word2describe = 30
 vocabulary_size = 4000
 image_size = 96
+net_name = 'storyNet_30'
